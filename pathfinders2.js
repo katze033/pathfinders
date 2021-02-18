85380f73aa2d392656297dfbbb9209e167f27a33
katze033 <joshkatzenmeyer@gmail.com>
2021-02-17 22:49:22 -0600

    Additional work on brushes.

--- pathfinders2.js
+++ pathfinders2.js
@@ -144,9 +144,9 @@ function drawTraveler() {
     **EXCLUSION, *SCREEN, **HARD_LIGHT, 
     **SOFT_LIGHT, **DODGE*/
     let x1, y1, x2, y2
-    let r = random(0,99)
-    
-    for (let i = 0; i < random(2,16); i++) {
+    let r = random(0, 99)
+
+    for (let i = 0; i < random(2, 16); i++) {
         if (r < 50) {
             blendMode(DIFFERENCE)
         } else {
@@ -154,61 +154,60 @@ function drawTraveler() {
         }
 
         // DIFFERENCE or SOFT_LIGHT
-        //filter(INVERT)
         rectMode(CENTER)
         strokeWeight(random(1, 12) * multiplier)
         stroke(travelerStroke)
         fill(travelerFill)
-        x1 = random(1920*multiplier, 2225*multiplier)
-        y1 = random(height -300 * multiplier, height -480*multiplier)
+        x1 = random(1920 * multiplier, 2225 * multiplier)
+        y1 = random(height - 300 * multiplier, height - 480 * multiplier)
         x2 = random(25 * multiplier, 75 * multiplier)
         y2 = random(25 * multiplier, 75 * multiplier)
         let c = random(0 * multiplier, 25 * multiplier)
         rect(x1, y1, x2, y2, c)
-        rect(4080*multiplier - x1, y1, x2, y2, c)
+        rect(4080 * multiplier - x1, y1, x2, y2, c)
 
-        
+
     }
 
     blendMode(BLEND)
-    
-    n = random(0,99)
-    
+
+    n = random(0, 99)
+
     if (n < 40) {
         stroke(secondary)
     } else {
         stroke(primary)
     }
-    for (let i = 0; i < random(1,10); i++) { //1,10
-        strokeWeight(random(1,3) * multiplier)
-        
-/*
-        width 1800, 2280 max?
-        let x1 = 1850*multiplier
-        let y1 = 2040*multiplier
-        let x2 = 2230*multiplier
-        let y2 = y1
-        line(x1,y1,x2,y2)
-  */      
-        let x1 = random(1820*multiplier, 2040*multiplier)
-        let y1 = random(1880 *multiplier,2040*multiplier)
+    for (let i = 0; i < random(1, 10); i++) { //1,10
+        strokeWeight(random(1, 3) * multiplier)
+
+        /*
+                width 1800, 2280 max?
+                let x1 = 1850*multiplier
+                let y1 = 2040*multiplier
+                let x2 = 2230*multiplier
+                let y2 = y1
+                line(x1,y1,x2,y2)
+          */
+        let x1 = random(1820 * multiplier, 2040 * multiplier)
+        let y1 = random(1880 * multiplier, 2040 * multiplier)
         let x2 = x1
-        let y2 = 2040*multiplier
+        let y2 = 2040 * multiplier
         line(x1, y1, x2, y2)
-        line(4080*multiplier - x1, y1, 4080*multiplier - x2, y2)
-        line(x1, 4080*multiplier - y1, x2, y2)
-        line(4080*multiplier - x1, 4080*multiplier - y1, 4080*multiplier - x2, y2)
-        x1 = random(1840*multiplier, 2040*multiplier)//1820*multiplier;
-        y1 = random(1840*multiplier, 2040*multiplier)
-        x2 = 2040*multiplier
+        line(4080 * multiplier - x1, y1, 4080 * multiplier - x2, y2)
+        line(x1, 4080 * multiplier - y1, x2, y2)
+        line(4080 * multiplier - x1, 4080 * multiplier - y1, 4080 * multiplier - x2, y2)
+        x1 = random(1840 * multiplier, 2040 * multiplier)//1820*multiplier;
+        y1 = random(1840 * multiplier, 2040 * multiplier)
+        x2 = 2040 * multiplier
         y2 = y1
-        line(x1,y1,x2,y2)
-        line(4080*multiplier - x1, y1, 4080*multiplier - x2, y2)
-        line(x1,4080*multiplier - y1,x2,4080*multiplier - y2)
-        line(4080*multiplier - x1,4080*multiplier - y1,4080*multiplier - x2,4080*multiplier - y2)
-    
+        line(x1, y1, x2, y2)
+        line(4080 * multiplier - x1, y1, 4080 * multiplier - x2, y2)
+        line(x1, 4080 * multiplier - y1, x2, 4080 * multiplier - y2)
+        line(4080 * multiplier - x1, 4080 * multiplier - y1, 4080 * multiplier - x2, 4080 * multiplier - y2)
+
     }
-    
+
 }
 
 let t
@@ -233,12 +232,12 @@ function drawBeziers() {
         ellipse(x1 + i * 2, y1 + i * 2, -x3 / 100 )
         circle(x1,y1 - Math.tan(millis() * 0.01) * 20,-y4 * Math.cos(millis() * 0.001)  + (i * 2))
         */
-        
+
         /*BRUSH2
         circle(x1,y1,-y4 * Math.cos(millis() * 0.0005)  + (i * 2))
         ellipse(x1 + i * 2, y1 + i * 2, -x3 / 100 )
         */
-        /*BRUSH3
+        /*BRUSH3 
         circle(x1,y1 - Math.sin(millis() * 0.01) * 100,-y4 * Math.cos(millis() * 0.0001)  + (i * 2))
         square(x1,y1,Math.sin(millis() * 0.001) * 40)
         */
@@ -252,34 +251,28 @@ function drawBeziers() {
        circle(x1,y1,-y4 * Math.cos(millis() * 0.001) / 200 + (i * 2) )
         */
 
-        /*BRUSH 6
+        /*BRUSH 6 (RARE?)
         triangle(x1 / Math.cos(millis(0.1)),y1,x2 - Math.tan(millis() * 0.0001) + i * 10,y2,x3,y3 - Math.tan(millis() * 0.0002) + (i * 2))
         square(x1 + i * 2,y1,height/24 - Math.tan(millis() * 0.0001) * 200)
         */
 
-        /*BRUSH 7
+        /*BRUSH 7 (RARE>)
         for (let i = 0; i<10; i++) {
             ellipse(x1 + i * 10, x2 + i * 10, Math.tan(millis() * 0.001) * 20 )
         }
         */
 
-        /* BRUSH 8
+        /*BRUSH 8 (RARE?)
        for (let i = 0; i<20; i++) {
         ellipse(x1 + i * 1, x2 + i * 1, Math.tan(millis() * 0.009) * 20 )
         } 
-        */ 
-        
-        
+         
         /*
-        beginShape(QUAD_STRIP)    
-            vertex(x1,y2 + (Math.sin(millis()*0.01) * 100))
-            vertex(y2,y2 - i * 2)
-            vertex(x3,y3)
-            vertex(x4,y4)
-        endShape()
+             for (let i = 0; i < 5; i++) {
+                line(x1 + i * 20, y1 + i * 200, x2 - i, y2)
+            }
         */
-        //line(x1 - i * 20,y1,x2,y2 + i * 1)
-
+        
     }
     t += 0.001;
 
@@ -294,12 +287,12 @@ function draw() {
     drawBeziers()
     resetMatrix()
     stroke(secondary)
-    strokeWeight(480 * multiplier)
+    strokeWeight(360 * multiplier)
     rect(width / 2, height / 2, width, height)
     fill(secondary)
     stroke(primary)
     strokeWeight(10 * multiplier)
-    
+
     circle(width - 360 * multiplier, height - 360 * multiplier, width / 4)
 
     if (frameCount < 2) {
@@ -307,7 +300,7 @@ function draw() {
         setShadowContext()
         noFill()
         stroke(secondary)
-        square(width / 2, height / 2, width / 1.25)
+        square(width / 2, height / 2, width / 1.182)
         fill(primary)
         circle(width - 360 * multiplier, height - 360 * multiplier, width / 4)
 
@@ -322,7 +315,7 @@ function draw() {
         */
 
     }
-randomSeed(2)    
+    randomSeed(2)
     revertShadowContext()
     drawTraveler()
 
@@ -332,3 +325,4 @@ randomSeed(2)
 
 // shadow context on brush is fucking bonkers
 // change terminal and purple palettes
+// now combine brushes (start with 6 & 8)
